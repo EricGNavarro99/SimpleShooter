735b1e69ddf76fa2d6d5e174609967cc38eaf3b9
Eric G. Navarro <ericgnavarro99@gmail.com>
2022-10-10 11:16:59 +0200

    New changes:
    - Creation of ShooterCharacter class.
    - Creation of ShooterCharacter blueprint.
    - Creation of map and set it to default.
    - Set BP_ShooterCharacter in new BP_ShooterGameModeClass.

---
+++ SimpleShooter/Source/SimpleShooter/ShooterCharacter.h
@@ -0,0 +1,25 @@
+// Created by Eric G. Navarro
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "ShooterCharacter.generated.h"
+
+UCLASS()
+class SIMPLESHOOTER_API AShooterCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AShooterCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
